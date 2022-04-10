3f3664956ca0ad8698540b5fab1adcb4563202e8
JaKyung <57945595+newmade01@users.noreply.github.com>
2022-04-10 20:08:17 +0900

    HyperParameter

--- Inference/Yolo_Efficient_Inference_function.py
+++ Inference/Yolo_Efficient_Inference_function.py
@@ -38,7 +38,7 @@ def Yolo():
     parser.add_argument('--weights', nargs='+', type=str, default='yolov5-V5.0_best_weight_batch16_epoch20.pt', help='model.pt path(s)') #best.pt로 실행
     parser.add_argument('--source', type=str, default='KAGGLE-IMAGE/archive/TEST_YOLO', help='source')  # Inference할 이미지, 동영상의 위치
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')   #정확도가 X이상일 때만, bounding box 표출
     parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--view-img', action='store_true', help='display results')
